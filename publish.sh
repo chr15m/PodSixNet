b31f65034e896f04c15120bec3dd4c1fb7f781d5
mccormix <mccormix@1a40dc54-9a4f-11de-bff5-93fd3790126f>
2009-09-05 19:15:42 +0000

    More publish script fixes.
    git-svn-id: http://podsixnet.googlecode.com/svn/podsixnet@47 1a40dc54-9a4f-11de-bff5-93fd3790126f

--- publish.sh
+++ publish.sh
@@ -2,20 +2,19 @@
 
 BZRREV=`bzr log -r -1.. --line | cut -d":" -f 1`
 SRV=mccormick.cx
-DIR=~/mccormick.cx/projects/PodSixNet/
+DIR='~/mccormick.cx/projects/PodSixNet/'
 REMOTE=$SRV:$DIR
 TARFILE=PodSixNet-$BZRREV.tar.gz
 
 echo "Generating and uploading HTML to mccormick.cx:"
-html=`markdown README`
 cp COPYING web/
-cp web/base.html web/index.html
-rpl -q '### content ###' "$html" web/index.html 2>/dev/null
-rpl -q '### version ###' "$BZRREV" web/index.html 2>/dev/null
+html=`markdown README`
+infile=`cat web/base.html`
+echo ${infile//\#\#\# content \#\#\#/$html} > web/index.html
+infile=`cat web/index.html`
+echo ${infile//\#\#\# version \#\#\#/$BZRREV} > web/index.html
 scp web/* $REMOTE
 
-#echo "Generating tarball and uploading to mccormick.cx:"
-#tar -c --exclude=*.tar.gz --exclude=*.pyc --exclude=.bzr --exclude=$TARFILE --exclude=web -zvf $TARFILE ../PodSixNet
 echo "Exporting tarball and uploading to mccormick.cx"
 bzr export PodSixNet-$BZRREV.tar.gz .
 scp PodSixNet-$BZRREV.tar.gz $REMOTE
