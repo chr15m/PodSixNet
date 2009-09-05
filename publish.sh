b8dac8df4780270752935d98475cbf56401f40c0
mccormix <mccormix@1a40dc54-9a4f-11de-bff5-93fd3790126f>
2009-09-05 19:12:46 +0000

    Website updating stuff.
    git-svn-id: http://podsixnet.googlecode.com/svn/podsixnet@31 1a40dc54-9a4f-11de-bff5-93fd3790126f

--- publish.sh
+++ publish.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+BZRREV=`bzr log -r -1.. --line | cut -d":" -f 1`
+REMOTE=mccormick.cx:~/mccormick.cx/projects/PodSixNet/
+TARFILE=PodSixNet-$BZRREV.tar.gz
+
 echo "Generating and uploading HTML to mccormick.cx:"
 html=`markdown README`
 cp web/base.html web/index.html
 rpl -q '### content ###' "$html" web/index.html 2>/dev/null
-scp web/* mccormick.cx:~/mccormick.cx/projects/PodSixNet/
+rpl -q '### version ###' "$BZRREV" web/index.html 2>/dev/null
+scp web/* $REMOTE
+
+echo "Generating tarball and uploading to mccormick.cx:"
+rm $TARFILE
+tar -c --exclude=.bzr --exclude=$TARFILE --exclude=web -zvf $TARFILE ../PodSixNet
+scp PodSixNet-$BZRREV.tar.gz $REMOTE
+
