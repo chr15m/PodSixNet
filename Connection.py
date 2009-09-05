f8a687cd2dfe0404b7203e59002095703754fafc
mccormix <mccormix@1a40dc54-9a4f-11de-bff5-93fd3790126f>
2009-09-05 19:08:22 +0000

    Refactoring Connection into EndPoint.
    git-svn-id: http://podsixnet.googlecode.com/svn/podsixnet@9 1a40dc54-9a4f-11de-bff5-93fd3790126f

--- Connection.py
+++ Connection.py
@@ -1,46 +1,8 @@
-from asyncore import poll2
-
 from PodSix.SelfCallMixin import SelfCallMixin
 
 from EndPoint import EndPoint
 
-class Connection(EndPoint):
-	def __init__(self, address=("127.0.0.1", 31425)):
-		self.address = address
-		self.isConnected = False
-		self.queue = []
-	
-	def DoConnect(self, address=None):
-		if not address:
-			address = self.address
-		EndPoint.__init__(self, address)
-	
-	def Close(self):
-		self.close()
-	
-	def GetQueue(self):
-		return self.queue
-	
-	def Pump(self):
-		self.queue = []
-		poll2()
-	
-	def Network_connected(self, data):
-		self.isConnected = True
-		self.queue.append({"action": "connected"})
-	
-	def Network(self, data):
-		self.queue.append(data)
-	
-	def Error(self, error):
-		self.queue.append({"action": "error", "error": error})
-	
-	def ConnectionError(self):
-		self.isConnected = False
-		EndPoint.ConnectionError(self)
-		self.queue.append({"action": "error", "error": "Connection error"})
-
-connection = Connection()
+connection = EndPoint()
 
 class ConnectionListener(SelfCallMixin):
 	"""
@@ -60,7 +22,7 @@ if __name__ == "__main__":
 		def Network_error(self, error):
 			print "error:", error['error']
 		
-		def Network_connected(self):
+		def Network_connected(self, data):
 			print "connection test Connected"
 	
 	c = ConnectionTest()
--- EndPoint.py
+++ EndPoint.py
@@ -1,16 +1,60 @@
-import asyncore
 import socket
+from asyncore import poll2
+
+from PodSix.SelfCallMixin import SelfCallMixin
 
 from Channel import Channel
 
 class EndPoint(Channel):
-	def __init__(self, addr):
+	"""
+	The endpoint queues up all network events for other classes to read.
+	"""
+	def __init__(self, address=("127.0.0.1", 31425)):
+		self.address = address
+		self.isConnected = False
+		self.queue = []
+	
+	def DoConnect(self, address=None):
+		if address:
+			self.address = address
 		Channel.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connect(addr)
+		self.connect(self.address)
+	
+	def Close(self):
+		self.isConnected = False
+		self.close()
+	
+	def GetQueue(self):
+		return self.queue
+	
+	def Pump(self):
+		self.queue = []
+		poll2()
+	
+	# methods to add network data to the queue depending on network events
+	
+	def Connected(self):
+		self.queue.append({"action": "socketConnect"})
 	
 	def Network_connected(self, data):
-		print 'got connected message', data
+		self.isConnected = True
+	
+	def Network(self, data):
+		self.queue.append(data)
+	
+	def Error(self, error):
+		self.queue.append({"action": "error", "error": error})
+	
+	def NetworkException(self):
+		self.queue.append({"action": "error", "error": 'Network exception'})
+	
+	def NetworkExceptionEvent(self):
+		self.queue.append({"action": "error", "error": 'Network exception event'})
+	
+	def ConnectionError(self):
+		self.isConnected = False
+		self.queue.append({"action": "error", "error": "Connection error"})
 
 if __name__ == "__main__":
 	from time import sleep
@@ -18,17 +62,16 @@ if __name__ == "__main__":
 		def Network_hello(self, data):
 			print "*Server* ran test method for 'hello' action"
 			print "*Server* received:", data
-	
-	class MyEndPoint(EndPoint):
-		def Network_connected(self, data):
-			print "*EndPoint* received connected"
-			print "*EndPoint* data:", data
+			self.Send({"action": "gotit", "data": "Yeah, we got it: " + str(len(data['data'])) + " elements"})
 	
 	print "Trying failing endpoint"
 	print "-----------------------"
 	endpoint_bad = EndPoint(("mccormick.cx", 23342))
+	endpoint_bad.DoConnect()
 	for i in range(50):
-		asyncore.poll2()
+		endpoint_bad.Pump()
+		if endpoint_bad.GetQueue():
+			print endpoint_bad.GetQueue()
 		sleep(0.001)
 	
 	from Server import Server
@@ -38,13 +81,18 @@ if __name__ == "__main__":
 	print "Trying successful endpoint"
 	print "--------------------------"
 	
-	endpoint = MyEndPoint(("localhost", 31425))
+	endpoint = EndPoint(("localhost", 31425))
+	endpoint.DoConnect()
 	endpoint.Send({"action": "hello", "data": {"a": 321, "b": [2, 3, 4], "c": ["afw", "wafF", "aa", "weEEW", "w234r"], "d": ["x"] * 256}})
 	endpoint.Send({"action": "hello", "data": [454, 35, 43, 543, "aabv"]})
 	endpoint.Send({"action": "hello", "data": [10] * 512})
 	
 	print "polling for half a second"
 	for x in range(50):
-		asyncore.poll2()
+		endpoint.Pump()
+		if endpoint.GetQueue():
+			print "*Endpoint*:", endpoint.GetQueue()
 		sleep(0.001)
-
+	
+	endpoint.Close()
+	print endpoint.GetQueue()
