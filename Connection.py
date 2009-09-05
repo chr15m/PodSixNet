c3fa22d6433d902dc8d0ea9c05b0197f26ee9fbf
mccormix <mccormix@1a40dc54-9a4f-11de-bff5-93fd3790126f>
2009-09-05 19:11:17 +0000

    Updated docs a bit.
    git-svn-id: http://podsixnet.googlecode.com/svn/podsixnet@24 1a40dc54-9a4f-11de-bff5-93fd3790126f

--- Connection.py
+++ Connection.py
@@ -1,9 +1,11 @@
 """
 A client's connection to the server.
 
+This module contains two components: a singleton called 'connection' and a class called 'ConnectionListener'.
+
 'connection' is a singleton instantiation of an EndPoint which will be connected to the server at the other end. It's a singleton because each client should only need one of these in most multiplayer scenarios. (If a client needs more than one connection to the server, a more complex architecture can be built out of instantiated EndPoint()s.) The connection is based on Python's asyncore and so it should have it's polling loop run periodically, probably once per gameloop. This just means putting "from Connection import connection; connection.Pump()" somewhere in your top level gameloop.
 
-Subclass ConnectionListener in order to have an object that will receive network events. For example, you might have a GUI element which updates a label saying how many players there are online. You would declare it like 'class NumPlayersLabel(ConnectionListener, ...):' Later you'd instantitate it 'n = NumPlayersLabel()' and then somewhere in your loop you'd have 'n.Pump()' which asks the connection singleton if there are any new messages from the network, and calls the 'Network_' callbacks for each bit of new data from the server. So you'd implement a method like "def Network_players(self, data):" which would be called whenever a message from the server arrived which looked like {"action": "players", "number": 5}.
+Subclass ConnectionListener in order to have an object that will receive network events. For example, you might have a GUI element which is a label saying how many players there are online. You would declare it like 'class NumPlayersLabel(ConnectionListener, ...):' Later you'd instantitate it 'n = NumPlayersLabel()' and then somewhere in your loop you'd have 'n.Pump()' which asks the connection singleton if there are any new messages from the network, and calls the 'Network_' callbacks for each bit of new data from the server. So you'd implement a method like "def Network_players(self, data):" which would be called whenever a message from the server arrived which looked like {"action": "players", "number": 5}.
 """
 
 from EndPoint import EndPoint
