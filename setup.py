c106b2c0794453142af7d8594c9d17b7421b0368
selfsame <joseph.parker.1@gmail.com>
2017-01-13 19:10:59 -0500

    Cleanup.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
 	name='PodSixNet',
-	description='PodSix multiplayer networking library for games',
+	description='Multiplayer networking library for games',
 	author='Chris McCormick',
 	author_email='chris@mccormick.cx',
-	url='http://mccormick.cx/projects/PodSixNet/',
+	url='https://github.com/chr15m/PodSixNet/',
 	packages=['PodSixNet'],
-	version=78,
+	version=79,
 )
 
