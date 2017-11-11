08a7f5107422ef7902ee7b628c1c2355ad946533
Chris McCormick <chris@mccormick.cx>
2017-11-11 19:59:33 +0800

    Release on PyPi.

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# convert readme to rst format
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except:
+   long_description = ''
 
 setup(
-	name='PodSixNet',
-	description='Multiplayer networking library for games',
-	author='Chris McCormick',
-	author_email='chris@mccormick.cx',
-	url='https://github.com/chr15m/PodSixNet/',
-	packages=['PodSixNet'],
-	version=79,
+    name='PodSixNet',
+    description='Multiplayer networking library for games',
+    author='Chris McCormick',
+    author_email='chris@mccormick.cx',
+    url='https://github.com/chr15m/PodSixNet',
+    packages=['PodSixNet'],
+    version="0.8",
 )
-
