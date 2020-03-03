f0ddae877e5aa26fb6ee3a9b8af4bc80c809ce7d
pauliyobo <pauliyobo@gmail.com>
2020-03-03 12:33:31 +0100

    test.py fixed, round 2 #26

--- PodSixNet/endpoint.py
+++ PodSixNet/endpoint.py
@@ -10,7 +10,7 @@ class EndPoint(Channel):
     """
     def __init__(self, address=("127.0.0.1", 31425), map=None):
         self.address = address
-        self.isConnected = False
+        self.is_connected = False
         self.queue = []
         if map is None:
             self._map = {}
@@ -25,10 +25,13 @@ class EndPoint(Channel):
             self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             self.connect(self.address)
+            return True
         except socket.gaierror as e:
             self.queue.append({"action": "error", "error": e.args})
+            return False
         except socket.error as e:
             self.queue.append({"action": "error", "error": e.args})
+            return False
     
     def get_queue(self):
         return self.queue
@@ -41,7 +44,7 @@ class EndPoint(Channel):
     # methods to add network data to the queue depending on network events
     
     def close(self):
-        self.isConnected = False
+        self.is_connected = False
         self.async_chat.close()
         self.queue.append({"action": "disconnected"})
     
@@ -49,7 +52,7 @@ class EndPoint(Channel):
         self.queue.append({"action": "socketConnect"})
     
     def Network_connected(self, data):
-        self.isConnected = True
+        self.is_connected = True
     
     def Network(self, data):
         self.queue.append(data)
@@ -57,7 +60,7 @@ class EndPoint(Channel):
     def Error(self, error):
         self.queue.append({"action": "error", "error": error})
     
-    def ConnectionError(self):
-        self.isConnected = False
+    def connection_error(self):
+        self.is_connected = False
         self.queue.append({"action": "error", "error": (-1, "Connection error")})
  
